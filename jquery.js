e6e750c169fcbe488b6c0009d121a12d2912e595
EmiCassin <74105850+EmiCassin@users.noreply.github.com>
2021-08-19 20:21:14 -0300

    Modificacion jquery

--- jquery.js
+++ jquery.js
@@ -1,1 +1,11 @@
 console.log("Hello World")
+
+const data = [
+    {nombre: "Emiliano", apellido: "Cassin"},
+    {nombre: "Timon", apellido: "Cassin"}
+]
+
+data.map(e =>{
+    console.log(e)
+}
+)
